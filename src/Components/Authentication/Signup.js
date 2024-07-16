5f3d5c7aee47ab7b1a6dcd8ddb7328f6b01427f5
jagdish245 <prjptjagadish61@gmail.com>
2024-07-16 19:50:07 +0530

    called user registration api

--- src/Components/Authentication/Signup.js
+++ src/Components/Authentication/Signup.js
@@ -1,39 +1,105 @@
-import React from 'react';
-import '../../Styles/signup.css'
+import { React, useState } from "react";
+import "../../Styles/signup.css";
+import axios from "axios";
 
 export default function Signup({ isOpen, onClose }) {
+  const [data, setData] = useState({
+    fullname: "",
+    email: "",
+    username: "",
+    password: "",
+    password1: "",
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      if (data.password === data.password1) {
+        delete data.password1;
+        const response = await axios.post("/api/users/register", data);
+        alert("Registration successful");
+        onClose();
+        console.log("Registration successful", response);
+      } else {
+        alert("Passwords don't match");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleChange(e) {
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
 
   if (!isOpen) return null;
   const handleCloseModal = (event) => {
     // Check if the click is outside of the modal content
-    if (event.target.classList.contains('modal-overlay')) {
+    if (event.target.classList.contains("modal-overlay")) {
       onClose();
-    }  
-  }; 
+    }
+  };
 
   return (
     <div className="modal-overlay" onClick={handleCloseModal}>
       <div className="modal-content">
-        <span className="close-button" onClick={onClose}>&times;</span>
+        <span className="close-button" onClick={onClose}>
+          &times;
+        </span>
         <h2>Register</h2>
-              <form>
-                  <label>Full Name</label>
-                    <input type="text" name="fullname" required placeholder='Enter Full Name'/>
+        <form onSubmit={handleSubmit} method="post">
+          <label>Full Name</label>
+          <input
+            type="text"
+            name="fullname"
+            value={data.fullname}
+            onChange={handleChange}
+            required
+            placeholder="Enter Full Name"
+          />
 
-                  <label>Email ID</label>
-                    <input type="email" name="email" required placeholder='Enter Email Address' />
+          <label>Email ID</label>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            required
+            placeholder="Enter Email Address"
+          />
 
-                  <label>Username</label>
-                    <input type="text" name="username" required placeholder='Enter Unique Username' />
-                 
-                  <label>Password</label>
-                    <input type="password" name="password1" placeholder="Enter your password" required/>
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={data.username}
+            onChange={handleChange}
+            required
+            placeholder="Enter Unique Username"
+          />
 
-                  <label>Confirm Password</label>
-                    <input type="password" name="password2" placeholder="Confirm your password" required/>
-                 
-                  <button type="submit">Signup</button>
-              </form>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+            required
+          />
+
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="password1"
+            value={data.password1}
+            onChange={handleChange}
+            placeholder="Confirm your password"
+            required
+          />
+
+          <button type="submit">Signup</button>
+        </form>
       </div>
     </div>
   );
